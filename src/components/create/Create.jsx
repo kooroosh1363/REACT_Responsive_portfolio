613833e80ce62740612e3c21ab664f098d358be6
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-13 10:59:22 +0400

    create css codes

--- src/components/create/Create.jsx
+++ src/components/create/Create.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./Create.css"
 import Common from '../common/Common'
-
+import { BsQuote } from "react-icons/bs";
+import { FaArrowRight } from "react-icons/fa";
+import { creative } from '../../allData';
 const Create = () => {
     return (
         <>
@@ -9,6 +11,28 @@ const Create = () => {
                 <div className="container">
                     <div className="contentItem">
                         <Common title="Create"/>
+                        {creative.map((val) => {
+                            return (
+                                <>
+                                <div className="content flex">
+                                    <div className="leftContent">
+                                        <BsQuote className='quote'></BsQuote>
+                                        <h1>{val.heading}</h1>
+                                        <h3>{val.title}</h3>
+                                        <p>{val.desc}</p>
+
+                                        <button className='btn_init'>
+                                            Download Resume <FaArrowRight/>
+                                        </button>
+                                    </div>
+
+                                    <div className="rightContent">
+                                        <img src={val.cover} alt="" />
+                                    </div>
+                                </div>
+                                </>
+                            )
+                        })}
                     </div>
                 </div>
             </section>
