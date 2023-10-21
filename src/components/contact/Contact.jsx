c94d5313114ba5b882c805ddb378d73a4f5bf46f
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-21 12:54:47 +0400

    add new comment

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,65 +1,71 @@
-import React from 'react'
-import "./Contact.css"
-import Common from '../common/Common'
-import image1 from "../../images/slide3.png"
+import React from 'react';
+import "./Contact.css";
+import Common from '../common/Common';
+import image1 from "../../images/slide3.png";
 import { BiMap } from "react-icons/bi";
 import { BsEnvelopeAt } from "react-icons/bs";
 import { BsTelephoneInboundFill } from "react-icons/bs";
 
-
 const Contact = () => {
   return (
     <>
-      <section className='contact create '>
+      <section className='contact create'>
         <div className="container">
-            <div className="contentItem">
-                <Common title="Contact"/>
-                <div className="content flex">
-                    <div className="rightContent">
-                        <img src={image1} alt="" />
-                    </div>
+          <div className="contentItem">
+            {/* Common component for the title "Contact" */}
+            <Common title="Contact"/>
+            <div className="content flex">
+              <div className="rightContent">
+                <img src={image1} alt="" />
+              </div>
 
-                    <div className="leftContent">
-                        <span>
-                            <BiMap></BiMap>
-                            Lorem ipsum dolor sit amet.
-                        </span>
-                        <br/>
-                        <br/>
+              <div className="leftContent">
+                {/* Contact Address */}
+                <span>
+                  <BiMap></BiMap>
+                  Lorem ipsum dolor sit amet.
+                </span>
+                <br/>
+                <br/>
 
-                        <span>
-                            <BsEnvelopeAt className="icon_contact"></BsEnvelopeAt>
-                            Lorem ipsum@dolorsitamet.
-                        </span>
-                        <br/>
-                        <br/>
+                {/* Contact Email */}
+                <span>
+                  <BsEnvelopeAt className="icon_contact"></BsEnvelopeAt>
+                  Lorem ipsum@dolorsitamet.
+                </span>
+                <br/>
+                <br/>
 
-                        <span>
-                            <BsTelephoneInboundFill className="icon_contact"></BsTelephoneInboundFill>
-                            +000000000000000
-                        </span>
-                        
+                {/* Contact Phone */}
+                <span>
+                  <BsTelephoneInboundFill className="icon_contact"></BsTelephoneInboundFill>
+                  +000000000000000
+                </span>
+                
+                <form>
+                  {/* Input for Name */}
+                  <input type="text" placeholder='Name...' id=''/>
+                  {/* Input for Email */}
+                  <input type="text" placeholder='Email...' id=''/>
+                  {/* Input for Interest */}
+                  <input type="text" placeholder='interested...' id=''/>
 
-                        <form>
-                            <input type="text"  placeholder='Name...' id=''/>
-                            <input type="text"  placeholder='Email...' id=''/>
-                            <input type="text"  placeholder='interested...' id=''/>
-
-                            <textarea  cols="30" rows="10">
-                                Message : 
-                            </textarea>
-                            <button className='btn_init'>
-                                Submit
-                            </button>
-                        </form>
-                    </div>
-                </div>
+                  {/* Textarea for Message */}
+                  <textarea cols="30" rows="10">
+                    Message : 
+                  </textarea>
+                  {/* Submit Button */}
+                  <button className='btn_init'>
+                    Submit
+                  </button>
+                </form>
+              </div>
             </div>
+          </div>
         </div>
       </section>
     </>
-  )
+  );
 }
 
-
-export default Contact
+export default Contact;
